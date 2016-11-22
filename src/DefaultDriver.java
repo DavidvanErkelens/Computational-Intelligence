3e63bcfab288d360942cdecc1417722415e2ec05
David van Erkelens <me@davidvanerkelens.nl>
2016-11-22 15:10:42 +0100

    first commit

---
+++ src/DefaultDriver.java
@@ -0,0 +1,90 @@
+import cicontest.algorithm.abstracts.AbstractDriver;
+import cicontest.algorithm.abstracts.DriversUtils;
+import cicontest.torcs.controller.extras.ABS;
+import cicontest.torcs.controller.extras.AutomatedClutch;
+import cicontest.torcs.controller.extras.AutomatedGearbox;
+import cicontest.torcs.controller.extras.AutomatedRecovering;
+import cicontest.torcs.genome.IGenome;
+import scr.Action;
+import scr.SensorModel;
+
+public class DefaultDriver extends AbstractDriver {
+
+    private NeuralNetwork neuralNetwork;
+
+    public DefaultDriver() {
+        initialize();
+        neuralNetwork = new NeuralNetwork(12, 8, 2);
+//        neuralNetwork = neuralNetwork.loadGenome();
+    }
+
+    private void initialize() {
+        this.enableExtras(new AutomatedClutch());
+        this.enableExtras(new AutomatedGearbox());
+        this.enableExtras(new AutomatedRecovering());
+        this.enableExtras(new ABS());
+    }
+
+    @Override
+    public void loadGenome(IGenome genome) {
+        if (genome instanceof DefaultDriverGenome) {
+            DefaultDriverGenome myGenome = (DefaultDriverGenome) genome;
+        } else {
+            System.err.println("Invalid Genome assigned");
+        }
+    }
+
+    @Override
+    public double getAcceleration(SensorModel sensors) {
+        double[] sensorArray = new double[4];
+        double output = neuralNetwork.getOutput(sensors);
+        return 1;
+    }
+
+    @Override
+    public double getSteering(SensorModel sensors) {
+        Double output = neuralNetwork.getOutput(sensors);
+        return 0.0;
+    }
+
+    @Override
+    public String getDriverName() {
+        return "TD Controller v1";
+    }
+
+    @Override
+    public Action controlWarmUp(SensorModel sensors) {
+        Action action = new Action();
+        return defaultControl(action, sensors);
+    }
+
+    @Override
+    public Action controlQualification(SensorModel sensors) {
+        Action action = new Action();
+        return defaultControl(action, sensors);
+    }
+
+    @Override
+    public Action controlRace(SensorModel sensors) {
+        Action action = new Action();
+        return defaultControl(action, sensors);
+    }
+
+    @Override
+    public Action defaultControl(Action action, SensorModel sensors) {
+        if (action == null) action = new Action();
+
+        action.steering = this.getSteering(sensors);
+        action.steering = 0.5;
+        action.accelerate = this.getAcceleration(sensors);
+
+
+
+        System.out.println("--------------" + getDriverName() + "--------------");
+        System.out.println("Steering: " + action.steering);
+        System.out.println("Acceleration: " + action.accelerate);
+        System.out.println("Brake: " + action.brake);
+        System.out.println("-----------------------------------------------");
+        return action;
+    }
+}
